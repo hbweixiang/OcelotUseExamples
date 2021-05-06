35030413ab6684ea0cec9550b7931769cbffec99
root <hbweixiang@outlook.com>
2021-05-06 18:29:44 +0800

    init project

---
+++ ApiExamples/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiExamples.Controllers
+{
+    [ApiController]
+    [Route("api/ocelot")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return $"This is from {HttpContext.Request.Host.Value}, path: {HttpContext.Request.Path}";
+        }
+    }
+}
